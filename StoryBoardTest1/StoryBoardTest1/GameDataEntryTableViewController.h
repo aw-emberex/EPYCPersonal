ab3c1fa944db344fae206d6867a704ea9ed4f75d
Alex Wait <alex.wait@emberex.com>
2013-05-13 22:28:54 -0700

    more work on game data entries view

---
+++ StoryBoardTest1/StoryBoardTest1/GameDataEntryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameDataEntryTableViewController.h
+//  EPYCPersonal
+//
+//  Created by Alex Wait on 5/13/13.
+//  Copyright (c) 2013 Emberex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameDataEntryTableViewController : UITableViewController
+
+@end
